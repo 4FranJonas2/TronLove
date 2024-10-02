77ba921068216ebb3202149b63d9bcedf07c48c6
Francisco <franjonas98@hotmail.com>
2024-10-02 10:25:40 -0300

    remove player stats draw from main

--- TronLove/main.lua
+++ TronLove/main.lua
@@ -7,12 +7,6 @@ end
 function love.draw()
     love.graphics.print("Cacho is everywhere", 400, 300)
     love.graphics.setBackgroundColor(0,0,0)
-    drawStats(player)
+   
 
-end
-
-function drawStats(player)
-    love.graphics.print("Player 1", 10, 20)
-    love.graphics.print("Pos X:", 10, 30, player.posX)
-    love.graphics.print("Pos Y:", 10, 40, player.posY)
 end
