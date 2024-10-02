7150cbf9e0e7babdda39eab26e2d3e9cc2696cc4
Francisco <franjonas98@hotmail.com>
2024-10-02 10:24:32 -0300

    fix player pos line code sintax error

--- TronLove/main.lua
+++ TronLove/main.lua
@@ -1,17 +1,7 @@
 function love.load()
-
-
-
-
-player = {
-    posX = 0,
-    posY = 0,
-    speed = 12.5,
-}
 end
 
 function love.update()
-
 end
 
 function love.draw()
@@ -23,6 +13,6 @@ end
 
 function drawStats(player)
     love.graphics.print("Player 1", 10, 20)
-    love.graphics.print("Pos X:", 10, 30... player.posX)
-    love.graphics.print("Pos Y:", 10, 40... player.posY)
+    love.graphics.print("Pos X:", 10, 30, player.posX)
+    love.graphics.print("Pos Y:", 10, 40, player.posY)
 end
