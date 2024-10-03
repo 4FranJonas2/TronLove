f66a78299fac6bbbce8ada453cd062e52cad24e6
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-02 21:23:00 -0300

    Update Bike to Knight

---
+++ TronLove/Knight.lua
@@ -0,0 +1,21 @@
+
+function CreateKnight(speed, position, image)
+    local knight = {}
+
+    knight.speed = 0
+    knight.position = position
+
+    knight.image = image
+
+    return knight
+end
+
+function MoveKnight(knight, x, y, dt)
+    knight.position.y = knight.position.y + knight.speed * dt * y
+    knight.position.x = knight.position.x + knight.speed * dt * x
+   return knight
+end
+
+function DrawBike(knight)
+    love.graphics.draw(knight.image, knight.position.x, knight.position.y)
+end
