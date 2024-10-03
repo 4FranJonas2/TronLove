e7a33a8fa7e02fcc2fda0f9ebe1a3186fe00aed3
Francisco <franjonas98@hotmail.com>
2024-10-03 14:34:57 -0300

    add mouse file and logic for menu

--- TronLove/main.lua
+++ TronLove/main.lua
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+local SceneGameplay = require ("sceneGameplay")
 
 local game = {
     state = {
@@ -6,37 +6,52 @@ local game = {
         running = false,
         endGamePause = false,
         ended = false
-    },
+    }
 }
-
 local mouse = {
     x = 30,
     y = 30
 }
 
 function love.load()
-    love.window.setTitle("Game name")
-    mouseBall = love.graphics.newImage("res/MouseBall.png")
-   mainMenu = require 'mainMenu'
 
+    --ANIM8
+    anim8 = require ("libraries/anim8")
+    love.graphics.setDefaultFilter("nearest","nearest")
+
+    --INIT SETTINGS
+    love.window.setTitle("Game name")
+    mouseBall.spriteSheet = love.graphics.newImage("res/Mouse2.png")
+    mouseBall.grid = anim8.newGrid(16, 16, mouseBall.spriteSheet:getWidth(), mouseBall.spriteSheet:getHeight())
+
+    mouseBall.animation = {}
+    mouseBall.animation.idle = anim8.newAnimation(mouseBall.grid('1-8', 1), 0.2)
+
+    mainMenu = require 'mainMenu'
+    love.mouse.setVisible(false)
+    --INIT PLAYERS
 end
 
-function love.update()
+function love.update(dt)
+    
     mouse.x, mouse.y = love.mouse.getPosition()
+    mouseBall.animation.idle:update(dt)
+
 end
 
 function love.draw()
     love.graphics.setBackgroundColor(0,0,0)
     mainMenu.drawMenu()
-    
+    screenWidth = love.graphics.getWidth()
+    screenHeight = love.graphics.getHeight()
+
     if  game.state["menu"]then
-        love.graphics.draw(mouseBall, mouse.x, mouse.y, 10)
+        --love.graphics.draw(mouseBall.spriteSheet, mouse.x, mouse.y, 10)
+        mouseBall.animation.idle:draw(mouseBall.spriteSheet, mouse.x, mouse.y, nill, 10)
     end
 
     if  game.state["running"]then
-            
+           
+        --player1.DrawKnight()    
     end
 end
-=======
-require ("SceneManager")
->>>>>>> 42b2df7c6ad79e3624e00fa439ef164e67da1df0
---
+++ TronLove/mouse.lua
@@ -0,0 +1,32 @@
+local mouseMenu = {}
+
+local mouse = {
+    x = 30,
+    y = 30
+}
+
+function mouseMenu.Init()
+
+    mouseBall = {}
+    mouseBall.spriteSheet = love.graphics.newImage("res/Mouse2.png")
+    mouseBall.grid = anim8.newGrid(16, 16, mouseBall.spriteSheet:getWidth(), mouseBall.spriteSheet:getHeight())
+
+    mouseBall.animation = {}
+    mouseBall.animation.idle = anim8.newAnimation(mouseBall.grid('1-8', 1), 0.2)
+
+end
+
+function mouseMenu.Update(dt)
+    mouse.x, mouse.y = love.mouse.getPosition()
+    mouseBall.animation.idle:update(dt)
+
+end
+
+function mouseMenu.Draw()
+
+    --love.graphics.draw(mouseBall.spriteSheet, mouse.x, mouse.y, 10)
+    mouseBall.animation.idle:draw(mouseBall.spriteSheet, mouse.x, mouse.y, nill, 10)
+
+end
+
+return mouseMenu
